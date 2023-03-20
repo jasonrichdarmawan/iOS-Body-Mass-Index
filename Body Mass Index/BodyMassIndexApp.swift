c6ddfb22c60daa5ac0b58eac5175b53638d5d74d
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-03-20 21:33:08 +0700

    add Calculator and History view

---
+++ Body Mass Index/BodyMassIndexApp.swift
@@ -0,0 +1,18 @@
+//
+//  Body_Mass_IndexApp.swift
+//  Body Mass Index
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BodyMassIndexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            // by using the `constant` function, we create a binding to a constant value that cannot be modified, which is suitable for previewing the view.
+            CalculatorView(weight: .constant(70), height: .constant(172), savedHistories: .constant([]))
+        }
+    }
+}
---
+++ Body Mass Index/History/DetailView.swift
@@ -0,0 +1,14 @@
+//
+//  DetailView.swift
+//  Body Mass Index
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 20/03/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
