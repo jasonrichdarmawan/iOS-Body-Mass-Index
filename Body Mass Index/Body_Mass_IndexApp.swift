6336116faa0d04923f5bbf2647d953b6a2c5efe2
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-03-20 14:25:54 +0700

    Initial Commit

---
+++ Body Mass Index/Body_Mass_IndexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Body_Mass_IndexApp.swift
+//  Body Mass Index
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Body_Mass_IndexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Body Mass Index/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Body Mass Index
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 20/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
